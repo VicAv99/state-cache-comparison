b49fe05004166e9f406f8f4052b3dab9b84d1ee5
Victor Avila <vavilamo@cisco.com>
2023-02-03 10:19:38 -0700

    feat: configure tailwind

---
+++ tailwind-workspace-preset.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ],
+};
