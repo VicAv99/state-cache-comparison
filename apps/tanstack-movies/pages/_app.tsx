87acdfee2245b6b7757ce3bf53a624783fea2038
Victor Avila <vicav99@gmail.com>
2023-02-03 20:53:58 -0700

    style: more layout

--- apps/tanstack-movies/pages/_app.tsx
+++ apps/tanstack-movies/pages/_app.tsx
@@ -1,6 +1,8 @@
+import './styles.css';
+
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+
 import { trpc } from '../utils/trpc';
 
 function CustomApp({ Component, pageProps }: AppProps) {
@@ -10,9 +12,9 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>Welcome to tanstack-movies!</title>
       </Head>
       <div className="bg-gray-50">
-        <div className="shadow-md relative bg-white">
-          <div className="w-full px-4 py-2 bg-white container">
-            <h3 className="m-0 p-0 font-mono">TanStack Movies</h3>
+        <div className="relative bg-white shadow-md">
+          <div className="container w-full px-4 py-2">
+            <h3 className="p-0 m-0 font-mono">TanStack Movies</h3>
           </div>
         </div>
         <main className="h-[calc(100vh-55px)] w-full container py-8">
--- apps/tanstack-movies/pages/index.tsx
+++ apps/tanstack-movies/pages/index.tsx
@@ -1,46 +1,80 @@
-import { trpc } from '../utils/trpc';
 import {
   Avatar,
   AvatarFallback,
   AvatarImage,
+  Button,
+  Input,
+  Label,
 } from '@state-cache-comparison/shared/ui';
+import { X } from 'lucide-react';
+
+import { trpc } from '../utils/trpc';
 
 export function Index() {
-  const hello = trpc.example.hello.useQuery({ text: 'client' });
+  const movies = trpc.movies.all.useQuery();
 
-  if (!hello.data) {
+  if (!movies.data) {
     return <div>Loading...</div>;
   }
 
   return (
     <div className="grid grid-cols-2 gap-3">
-      <div className="bg-white rounded-md p-4">
+      <div className="p-4 bg-white rounded-md">
         <ul className="max-w-md divide-y divide-gray-700">
-          <li className="pb-3 sm:pb-4">
-            <div className="flex items-center space-x-4">
-              <div className="flex-shrink-0">
-                <Avatar>
-                  <AvatarImage
-                    src="https://github.com/shadcn.png"
-                    alt="@shadcn"
-                  />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-              </div>
-              <div className="flex-1 min-w-0">
-                <p className="text-sm font-medium text-gray-900 truncate">
-                  Neil Sims
-                </p>
-                <p className="text-sm text-gray-500 truncate">
-                  email@flowbite.com
-                </p>
-              </div>
-              <div className="inline-flex items-center text-base font-semibold text-gray-900">
-                $320
-              </div>
-            </div>
-          </li>
+          {movies?.data &&
+            movies?.data?.movies.map((movie) => (
+              <li key={movie.id} className="p-3">
+                <div className="rounded-md">
+                  <div className="flex items-center space-x-4">
+                    <div className="flex-shrink-0">
+                      <Avatar>
+                        <AvatarImage src={movie.image} alt="@shadcn" />
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+                    </div>
+                    <div className="flex-1 min-w-0">
+                      <p className="text-sm font-medium text-gray-900 truncate">
+                        {movie.title}
+                      </p>
+                      <p className="text-sm text-gray-500 truncate">
+                        {movie.releaseDate.toDateString()}
+                      </p>
+                    </div>
+                    <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                      <Button>
+                        <X className="w-4 h-4" />
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
         </ul>
+      </div>
+      <div className="p-4 bg-white rounded-md">
+        <h3>Movies</h3>
+        <div className="my-3 space-y-2">
+          <div className="grid w-full items-center gap-1.5">
+            <Label htmlFor="title">Title</Label>
+            <Input type="text" id="title" placeholder="Title" />
+            <p className="text-sm text-slate-500"></p>
+          </div>
+          <div className="grid w-full items-center gap-1.5">
+            <Label htmlFor="overview">Overview</Label>
+            <Input type="text" id="overview" placeholder="Overview" />
+            <p className="text-sm text-slate-500"></p>
+          </div>
+          <div className="grid w-full items-center gap-1.5">
+            <Label htmlFor="release-date">Release Date</Label>
+            <Input type="text" id="release-date" placeholder="Release Date" />
+            <p className="text-sm text-slate-500"></p>
+          </div>
+          <div className="grid w-full items-center gap-1.5">
+            <Label htmlFor="image">Image</Label>
+            <Input type="text" id="image" placeholder="Image" />
+            <p className="text-sm text-slate-500"></p>
+          </div>
+        </div>
       </div>
     </div>
   );
