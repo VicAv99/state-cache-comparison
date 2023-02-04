65460c6e8bec44050055cd72a406595a752f242a
Victor Avila <vavilamo@cisco.com>
2023-02-03 19:56:25 -0700

    styling and ui

--- apps/tanstack-movies/pages/_app.tsx
+++ apps/tanstack-movies/pages/_app.tsx
@@ -9,9 +9,16 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Welcome to tanstack-movies!</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <div className="bg-gray-50">
+        <div className="shadow-md relative bg-white">
+          <div className="w-full px-4 py-2 bg-white container">
+            <h3 className="m-0 p-0 font-mono">TanStack Movies</h3>
+          </div>
+        </div>
+        <main className="h-[calc(100vh-55px)] w-full container py-8">
+          <Component {...pageProps} />
+        </main>
+      </div>
     </>
   );
 }
--- apps/tanstack-movies/pages/index.tsx
+++ apps/tanstack-movies/pages/index.tsx
@@ -1,13 +1,47 @@
 import { trpc } from '../utils/trpc';
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from '@state-cache-comparison/shared/ui';
 
 export function Index() {
   const hello = trpc.example.hello.useQuery({ text: 'client' });
+
   if (!hello.data) {
     return <div>Loading...</div>;
   }
+
   return (
-    <div>
-      <p>{hello.data.greeting}</p>
+    <div className="grid grid-cols-2 gap-3">
+      <div className="bg-white rounded-md p-4">
+        <ul className="max-w-md divide-y divide-gray-700">
+          <li className="pb-3 sm:pb-4">
+            <div className="flex items-center space-x-4">
+              <div className="flex-shrink-0">
+                <Avatar>
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="@shadcn"
+                  />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </div>
+              <div className="flex-1 min-w-0">
+                <p className="text-sm font-medium text-gray-900 truncate">
+                  Neil Sims
+                </p>
+                <p className="text-sm text-gray-500 truncate">
+                  email@flowbite.com
+                </p>
+              </div>
+              <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                $320
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
