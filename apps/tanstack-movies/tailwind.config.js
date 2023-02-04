4f8bd1324d14ad2db8813e2c1b9e3ed7cd1f43b7
Victor Avila <vavilamo@cisco.com>
2023-02-03 19:56:03 -0700

    fix: tailwind again

--- apps/tanstack-movies/tailwind.config.js
+++ apps/tanstack-movies/tailwind.config.js
@@ -1,13 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
 const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
 const { join } = require('path');
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [require(join(__dirname, '../../tailwind-workspace-preset.js'))],
   content: [
     join(
       __dirname,
-      '{src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+      './{src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}'
     ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
