233f19baba0aca90a6ce79ebfda773110c55a831
Victor Avila <vicav99@gmail.com>
2023-02-04 17:57:01 -0700

    chore: generate apollo planets

---
+++ apps/apollo-planets/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'apollo-planets',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/apollo-planets',
+};
