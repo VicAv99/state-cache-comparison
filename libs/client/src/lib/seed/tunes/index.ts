9715f849dd3d53e082a9d60ec10de97728687ddf
Victor Avila <vicav99@gmail.com>
2023-02-04 20:58:34 -0700

    feat: add seed data for tunes

---
+++ libs/client/src/lib/seed/tunes/index.ts
@@ -0,0 +1,23 @@
+export const tunes = [
+  {
+    name: 'Welcome to the Jungle',
+    artist: 'Guns N’ Roses',
+    image:
+      'https://www.rollingstone.com/wp-content/uploads/2021/09/490-gnr-welcome-to-the-jungle.jpeg?w=306',
+    length: '2 min 30 sec',
+  },
+  {
+    name: 'Livin’ on a Prayer',
+    artist: 'Bon Jovi',
+    image:
+      'https://www.rollingstone.com/wp-content/uploads/2021/09/457-bon-jovi-living-on-a-prayer.jpg?w=500',
+    length: '2 min 30 sec',
+  },
+  {
+    name: 'Bad and Boujee',
+    artist: 'Migos ft. Lil Uzi Vert',
+    image:
+      'https://www.rollingstone.com/wp-content/uploads/2021/09/451-migos-bad-and-boujee.jpg?w=500',
+    length: '2 min 30 sec',
+  },
+];
