c9fba3e252986039daf330ea647261f68bb05e8f
Victor Avila <vicav99@gmail.com>
2023-02-05 19:04:38 -0700

    chore: more stuff

--- libs/client/src/lib/seed/teams/index.ts
+++ libs/client/src/lib/seed/teams/index.ts
@@ -3,25 +3,34 @@ export const teams = [
     name: 'Chargers',
     state: 'LA',
     slogan: 'Almost',
+    image:
+      'https://content.sportslogos.net/logos/7/6446/thumbs/644616602020.gif',
   },
   {
     name: 'Chiefs',
     state: 'Missouri',
     slogan: 'Run it back',
+    image: 'https://content.sportslogos.net/logos/7/162/thumbs/857.gif',
   },
   {
     name: 'Bills',
     state: 'Buffalo',
     slogan: 'Nobody Circles The Wagons Like The Buffalo Bills',
+    image:
+      'https://content.sportslogos.net/logos/7/149/thumbs/n0fd1z6xmhigb0eej3323ebwq.gif',
   },
   {
     name: 'Rams',
     state: 'LA',
     slogan: 'We not me',
+    image:
+      'https://content.sportslogos.net/logos/7/5941/thumbs/594183342020.gif',
   },
   {
     name: 'Cardinals',
     state: 'AZ',
     slogan: 'Rise Up Red Sea',
+    image:
+      'https://content.sportslogos.net/logos/7/177/thumbs/kwth8f1cfa2sch5xhjjfaof90.gif',
   },
 ];
